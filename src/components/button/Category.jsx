398c1448d6ccbe11d99be6619ee0a79b1d875464
donghoon4907 <wre789812@naver.com>
2022-07-15 18:02:40 +0900

    [Front] Recommand category - 3

--- src/components/button/Category.jsx
+++ src/components/button/Category.jsx
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
  * 카테고리 랜더링 컴포넌트
  *
  * @param {string}  props.content 카테고리명
+ * @param {string}  props.count   카테고리가 사용된 수
  * @param {boolean} props.isGap   간격 사용 여부
  */
 const CategoryBtn = ({ content, count, isGap }) => {
